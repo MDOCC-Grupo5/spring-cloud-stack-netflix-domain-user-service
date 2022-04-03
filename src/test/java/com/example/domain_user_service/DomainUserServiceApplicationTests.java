c3316ad4e333d683c3a8018f1750996835ffd7af
Juan Sebastian Suaza Solano <juan.suaza@api-ux.com>
2022-04-02 19:29:42 -0500

    first commit

---
+++ src/test/java/com/example/domain_user_service/DomainUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domain_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
